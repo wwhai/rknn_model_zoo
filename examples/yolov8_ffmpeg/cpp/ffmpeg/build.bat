4765b70d8bc686dc23f99698702b635e8cf04b98
wwhai <cnwwhai@gmail.com>
2024-08-17 19:34:32 +0800

    dev: add demo

--- examples/yolov8_ffmpeg/cpp/ffmpeg/build.bat
+++ examples/yolov8_ffmpeg/cpp/ffmpeg/build.bat
@@ -1,4 +1,9 @@
 gcc -o ffmpeg_rtsp_example.exe ffmpeg.c ^
     -IC:/Users/wangwenhai/workspace/ffmpeg-6.1.1-full_build-shared/include ^
     -LC:/Users/wangwenhai/workspace/ffmpeg-6.1.1-full_build-shared/bin ^
-    -lavformat-60 -lavcodec-60 -lavutil-58 -lswscale-7 -lm
+    -lavformat-60 -lavcodec-60 -lavutil-58 -lswscale-7 -lavfilter-9 -lm
+
+gcc -o avfilter-demo.exe avfilter-demo.c ^
+    -IC:/Users/wangwenhai/workspace/ffmpeg-6.1.1-full_build-shared/include ^
+    -LC:/Users/wangwenhai/workspace/ffmpeg-6.1.1-full_build-shared/bin ^
+    -lavformat-60 -lavcodec-60 -lavutil-58 -lswscale-7 -lavfilter-9 -lm
